dcf806b9911e94b404c7dffeef5db82e194a2487
Abhishek Kumar <abhishekkumar.mg121@webkul.in>
2025-09-02 12:41:10 +0530

    fix: fixed the issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+export const dynamic = "force-static";
+export const revalidate = 3600; // revalidate every 1 hours
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
